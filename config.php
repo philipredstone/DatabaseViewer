4ee9c91f06f04fd35233dbfbe88e58d9da39a410
Philip Rothstein <rothstein@prtex.de>
2020-06-11 13:43:30 +0200

    cleaned files the second time 🧹

--- config.php
+++ config.php
@@ -1,2 +1,14 @@
 <?php
-$servername = "127.0.0.1";
+/*************************************************************
+ * $server_name                                              *
+ * IP Adress or domain of the MySQL database                 *
+ * -> required                                               *
+ *************************************************************/
+$server_name = "localhost";
+
+/*************************************************************
+ * $brand_name                                               *
+ * The name that will be shown in the Website tile           *
+ * -> optional (if empty, no name will be shown)             *
+ *************************************************************/
+$brand_name = "My new Title";
--- index.php
+++ index.php
@@ -10,47 +10,107 @@ include("config.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php if(isset($_GET['database'])) {
+    <title>
+    <?php 
+    
+    if(isset($_GET['database']) && isset($_GET['table'])) {
+        echo $_GET['database']." ⮞ ".$_GET['table']." - ";
+    } else if(isset($_GET['database']) && !isset($_GET['table'])) {
         echo $_GET['database']." - ";
-    } ?>Database Viewer</title>
+    }
+    echo $brand_name;
+    ?></title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="./style.css" rel="stylesheet"> 
+    <link rel="apple-touch-icon" sizes="57x57" href="./favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
 </head>
 <body>
-    <a class="logout" href="./logout.php">logout</a>
 
-<?php
-
-if(isset($_POST['user']) && isset($_POST['pass'])) {
-    try {
-      $conn = new PDO("mysql:host=$servername", $_POST['user'], $_POST['pass']);
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $_SESSION["user"] = $_POST['user'];
-      $_SESSION["pass"] = $_POST['pass'];
-      header("Location: ./");
-    } catch(PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
-      session_unset();
-      header("Location: ./login.php");
-    }
-} else {
-    if(isset($_SESSION["user"]) && isset($_SESSION["pass"])) {
+<?php if(isset($_GET['logout'])) { 
+    session_unset();
+    header("Location: ./?login");
+} else if(isset($_GET['login'])) { ?>
+    <main>
+        <div class="wrapper-card">
+            <h2 style="font-weight: 900;">Log In</h2>
+            <h3>Please Log In to view the databases!</h3>
+            <form action="index.php" method="post">
+                <div class="text-field text-field--outlined full-width" style="max-width: 90%; left: 5%;">
+                    <input type="text" id="username-field" name="user" class="text-field__input" required>
+                    <div class="notched-outline notched-outline--upgraded notched-outline--notched">
+                        <div class="notched-outline__leading"></div>
+                        <div class="notched-outline__notch" style="width: 64px;">
+                            <label class="floating-label floating-label--float-above" for="my-text-field">Username</label>
+                        </div>
+                        <div class="notched-outline__trailing"></div>
+                    </div>
+                </div>  
+                <div class="text-field text-field--outlined full-width" style="max-width: 90%; left: 5%;">
+                    <input type="password" id="password-field" name="pass" class="text-field__input" required>
+                    <div class="notched-outline notched-outline--upgraded notched-outline--notched">
+                        <div class="notched-outline__leading"></div>
+                        <div class="notched-outline__notch" style="width: 62.25px;">
+                            <label class="floating-label floating-label--float-above" for="my-text-field">Password</label>
+                        </div>
+                        <div class="notched-outline__trailing"></div>
+                    </div>
+                </div> 
+                <input type="submit" style="position: absolute; left: -9999px; width: 1px; height: 1px;" tabindex="-1" />
+            </form>       
+            <div class="footer-wrapper" style="position: fixed; right: 5%; bottom: 5%;">
+            </div>
+         </div>
+    </main>
+<?php } else {
+    echo '<a class="note" href="./?logout" title="Logout"><span class="material-icons">exit_to_app</span></a><a class="note" onclick="change()" title="Toggle Theme (light/dark)"><span class="material-icons">brightness_6</span></a>';
+    js();
+    if(isset($_POST['user']) && isset($_POST['pass'])) {
         try {
-          $conn = new PDO("mysql:host=$servername", $_SESSION['user'], $_SESSION['pass']);
+          $conn = new PDO("mysql:host=$server_name", $_POST['user'], $_POST['pass']);
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          data($conn);
+          $_SESSION["user"] = $_POST['user'];
+          $_SESSION["pass"] = $_POST['pass'];
+          header("Location: ./");
         } catch(PDOException $e) {
           echo "Connection failed: " . $e->getMessage();
           session_unset();
-          header("Location: ./login.php");
+          header("Location: ./?login");
         }
     } else {
-        header("Location: ./login.php");
+        if(isset($_SESSION["user"]) && isset($_SESSION["pass"])) {
+            try {
+              $conn = new PDO("mysql:host=$server_name", $_SESSION['user'], $_SESSION['pass']);
+              $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              data($conn);
+            } catch(PDOException $e) {
+              echo "Connection failed: " . $e->getMessage();
+              session_unset();
+              header("Location: ./?login");
+            }
+        } else {
+            header("Location: ./?login");
+        }
     }
+
 }
-
-
 function data($conn) {
     if(!isset($_GET['database']) && !isset($_GET['table'])) {
         $query = $conn->prepare('SHOW DATABASES');
@@ -101,23 +161,38 @@ function data($conn) {
         }
     }
 }
+?>
+<br />
+<script>
+<?php
+ function js() {
+    echo "<script>if(localStorage.getItem('dark__') == \"true\") {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }</script>";
+ }
+
 
 
 ?>
 
-<br />
-<script>
+
+function change() {
+    document.body.classList.toggle('dark');
+        if(localStorage.getItem('dark__') == "true") {
+            localStorage.setItem('dark__', 'false');
+        } else {
+            localStorage.setItem('dark__', 'true');
+        }
+}
 
 function intable(element) {
-    <?php if(isset($_GET['database'])) {
-        echo 'window.location = "./?database='.$_GET['database'].'&table="+element';
-    } 
-    ?>
+    <?php if(isset($_GET['database'])) {echo 'window.location = "./?database='.$_GET['database'].'&table="+element';} ?>
 }
 function table(element) {
     window.location = "./?database="+element;
 }
 </script>
 </body>
-
 </html>
